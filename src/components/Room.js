1234b2cb704bb1a633449846bd984b9b22e2486c
Junior Klegseth <jklegseth@gmail.com>
2017-10-27 16:42:28 -0700

    add room functionality

--- src/components/Room.js
+++ src/components/Room.js
@@ -5,7 +5,10 @@ class Room extends Component {
     super(props);
 
     this.state = ({
-      rooms: []
+      rooms: [],
+      newRoomName: '',
+      hasError: false,
+      newRoomError: ''
     });
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -21,9 +24,51 @@ class Room extends Component {
     });
   }
 
+  updateRoomName(e) {
+    const roomName = e.target.value;
+    const hasError = !!roomName ? false : this.state.hasError;
+    const newRoomError = !!roomName ? '' : this.state.newRoomError;
+
+    this.setState({
+      newRoomName: roomName,
+      hasError: hasError,
+      newRoomError: newRoomError
+    });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    const roomName = this.state.newRoomName;
+
+    if (!roomName) {
+      return this.setState({
+        hasError: true,
+        newRoomError: 'Please enter a name'
+      });
+    }
+    this.roomsRef.push({
+      name: roomName
+    });
+    this.setState({
+      newRoomName: '',
+      hasError: false,
+      newRoomError: ''
+    });
+  }
+
   render() {
     return (
       <div className="room-list">
+        <form>
+          <input
+            type="text"
+            value={this.state.newRoomName}
+            onChange={this.updateRoomName.bind(this)}
+            placeholder="Enter new room name"
+          />
+          <button onClick={this.createRoom.bind(this)}>Add Room</button>
+          <span className={this.state.hasError ? 'form-error' : ''}>{this.state.newRoomError}</span>
+        </form>
         {
           this.state.rooms.map(room =>
             <li key={room.key}>
@@ -31,7 +76,6 @@ class Room extends Component {
             </li>
           )
         }
-
       </div>
     )
   }
